c137254e82bc132261f4baeb72029744b06291d0
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2021-08-05 03:17:28 -0300

    Add files via upload
    Creada la función para calcular el cambio
    Actualizada la sintaxis camel Case

--- coflaHeladeria/codigo.js
+++ coflaHeladeria/codigo.js
@@ -9,6 +9,8 @@ document.write(`<p>Hola <b>${usuario}</b>. güelcam tu de solushion wan</p>`);
 
 alert("Hola " + usuario +" en la próxima ventana, ingresa solo valores numéricos, sin símbolos");
 
+let en = false;
+const bobo=23;
 
 // let dineroRoberto = parseInt(prompt(`Roberto, cuanto dinero tiene? `));
 // let dineroPedro = parseInt(prompt(`Pedro, cuanto dinero tiene? `));
@@ -23,9 +25,16 @@ let dineroObj = {
 }
 	
 
-document.write("Dinero de los gurises: <br>")
+document.write(`-----------<br>MONEY <br>
+	            -----------<br>`)
 for (let nombre in dineroObj){
-	document.write(`${nombre} tiene ${dineroObj[nombre]} pesos <br>`);
+
+	if (dineroObj[nombre] !== 0 && dineroObj[nombre] !== 1) {
+		document.write(`${nombre} tiene ${dineroObj[nombre]} pesos <br>`);
+	}else {
+		document.write(`${nombre} tiene ${dineroObj[nombre]} peso <br>`);
+	}
+	
 }
 
 // let dineroArray = [
@@ -44,17 +53,17 @@ for (let nombre in dineroObj){
 // };
 
 let valorPalitoAgua = 6;
-let valorpalitoCrema = 9;
-let valorbombonHeladix = 12;
-let valorbombonHelardo = 12;
-let valorpoteConfites = 32;
+let valorPalitoCrema = 9;
+let valorBombonHeladix = 12;
+let valorBombonHelardo = 12;
+let valorPoteConfites = 32;
 
 const HeladosObj = {
 	palitoAgua: parseInt(valorPalitoAgua),
-	palitoCrema: parseInt(valorpalitoCrema),
-	bombonHeladix: parseInt(valorbombonHeladix),
-	bombonHelardo: parseInt(valorbombonHelardo),
-	poteConfites: parseInt(valorpoteConfites),
+	palitoCrema: parseInt(valorPalitoCrema),
+	bombonHeladix: parseInt(valorBombonHeladix),
+	bombonHelardo: parseInt(valorBombonHelardo),
+	poteConfites: parseInt(valorPoteConfites),
 
 	dameListadoHelados() {
 		document.write(`
@@ -78,10 +87,10 @@ const HeladosObj = {
 // let ingresaValorHelados= prompt("Quieres ingresar el valor de los helados? (S/N)");
 // if (ingresaValorHelados.toUpperCase()== "S"){
 // 	let valorPalitoAgua = prompt(`Cuánto cuesta el Palito de Agua? `);
-// 	let valorpalitoCrema = prompt(`Cuanto cuesta el Palito de Crema? `);
-// 	let valorbombonHeladix = prompt(`Cuánto cuesta el Bombón Heladix? `);
-// 	let valorbombonHelardo = prompt(`Cuánto cuesta el Bombón Helardo? `)
-// 	let valorpoteConfites = prompt(`Cuánto cuesta el Pote de confites? `);
+// 	let valorPalitoCrema = prompt(`Cuanto cuesta el Palito de Crema? `);
+// 	let valorBombonHeladix = prompt(`Cuánto cuesta el Bombón Heladix? `);
+// 	let valorBombonHelardo = prompt(`Cuánto cuesta el Bombón Helardo? `)
+// 	let valorPoteConfites = prompt(`Cuánto cuesta el Pote de confites? `);
 // } else {
 // 	//listo los helados
 // 	HeladosObj.dameListadoHelados();
@@ -89,36 +98,50 @@ const HeladosObj = {
 
 HeladosObj.dameListadoHelados();
 
+//calculamos el vuelto
+function vuelto (dinero, gasto) {
+		let cambio = (parseInt(dinero) - parseInt(gasto));
+		if (cambio == 0 ) {
+			return "Que esperás el cambio? no te sobra nada valor";
+		} 
+		else if (cambio == 1) {
+			return "Acá tenés el vuelto, te sobra un pesito";
+		}
+		else  {
+			 return `y encima te sobran ${cambio} pesos`;
+		}
+}
 
 
 //recorro un array y almaceno el dinero de cada uno
 for (i in dineroObj){
 	// document.write(`${i} es un ${typeof(i)}<br>`);
 	// document.write(`${dineroObj[i]}  es un ${typeof(dineroObj[i])}`);
-
-	if (dineroObj[i] >=valorPalitoAgua && dineroObj[i]< valorpalitoCrema){
+	if (dineroObj[i] >= valorPalitoAgua && dineroObj[i] < valorPalitoCrema){
 	document.write(
-		`${i} con esos ${dineroObj[i]} pesos te comprás el Palito de Agua y no jodés más.<br>
-		Te sobran ${(dineroObj[i] - valorPalitoAgua)} pesos <br><br>`);
+		`${i} con esos <b>${dineroObj[i]} pesos</b> te comprás el Palito de Agua y no jodés más.<br>
+		${vuelto(dineroObj[i], valorPalitoAgua)} <br><br>`);
 	}	
-	else if (dineroObj[i] >=valorpalitoCrema && dineroObj[i]< valorbombonHeladix){
-	document.write(
-		`${i} con esos ${dineroObj[i]} pesos te comprás el  Palito de Crema, no está mal,
-		 tiene un cacho de gusto a vainilla y te sobran ${(dineroObj[i] - valorPalitoAgua)} pesos <br><br>`);
+	else if (dineroObj[i] >= valorPalitoCrema && dineroObj[i] < valorBombonHeladix){
+		document.write(
+		`${i} con esos <b>${dineroObj[i]} pesos</b> te comprás el  Palito de Crema, no está mal,
+		 ${vuelto(dineroObj[i], valorPalitoCrema)} <br><br>`);
 	}
-	else if (dineroObj[i]  >=valorbombonHeladix && (dineroObj[i]) < valorpoteConfites) {
+	else if (dineroObj[i] >= valorBombonHeladix && dineroObj[i] < valorPoteConfites) {
 	document.write(`
-		${i} con esos ${dineroObj[i]} pesos te comprás un Bombón Heladix o un Bombón Helardo, están tremendos, la pasas bomba<br>
-		y te sobran ${(dineroObj[i] - valorbombonHeladix)} pesos <br><br>`);
+		${i} con esos <b>${dineroObj[i]} pesos</b> te comprás un Bombón Heladix o un Bombón Helardo, están tremendos<br>
+		${vuelto(dineroObj[i], valorBombonHeladix)} <br><br>`);
 		}
-	else if (dineroObj[i]   >=valorpoteConfites) {
+	else if (dineroObj[i] >= valorPoteConfites) {
 	document.write(
-		`${i} con esos ${dineroObj[i]} pesos te comprás un pote con confites, lo ponés al lado del teclado 
-		y sos uno solo con en el universo <br>
-		te sobran ${(dineroObj[i] - valorpoteConfites)} pesos <br><br>`);
-		}		
+		`${i} con esos <b>${dineroObj[i]} pesos</b> te comprás un pote con confites, lo ponés al lado del teclado 
+		y sos uno solo con en el universo... ommm...  ${vuelto(dineroObj[i], valorPoteConfites)} <br><br>`);
+	}
+	else if (dineroObj[i] < valorPalitoAgua && dineroObj[i] !== 0 && dineroObj[i] !== 1) {
+		document.write(`${i}, con esos <b>${dineroObj[i]} pesos</b> no tenés ni pa un helado, rajá de acá <br>`);
+	}		
 	else {
-		document.write(`${i}, con esos ${dineroObj[i]} pesos no tenés ni pa un helado, rajá de acá <br>`)
+		document.write(`${i}, con <b>${dineroObj[i]} peso</b> que querés hacer, arrancá como una moto <br>`)
 	}
 
 }
